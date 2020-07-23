2d71d3e337bbb342b28056248e85f926b896a843
MuthuGokila <satheesanmahalingam@gmail.com>
2020-07-23 13:22:35 +0530

    Updates

---
+++ precache-manifest.d472bff0aa79d7f0b92c04e18a7bf688.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff108edf5636bcf7e1af918dc15d078",
+    "url": "/HealthyDermaBeauty/index.html"
+  },
+  {
+    "revision": "c06defd8077954eb4a6e",
+    "url": "/HealthyDermaBeauty/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "567d2e0986ca1aef5b2c",
+    "url": "/HealthyDermaBeauty/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "c06defd8077954eb4a6e",
+    "url": "/HealthyDermaBeauty/static/js/2.293679eb.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/HealthyDermaBeauty/static/js/2.293679eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "567d2e0986ca1aef5b2c",
+    "url": "/HealthyDermaBeauty/static/js/main.e0103a97.chunk.js"
+  },
+  {
+    "revision": "30d72e54d615c1ead702",
+    "url": "/HealthyDermaBeauty/static/js/runtime-main.38a9f59c.js"
+  },
+  {
+    "revision": "0661d0cf2b2b521ddf8079a7081e42b1",
+    "url": "/HealthyDermaBeauty/static/media/CustomizedFacial.0661d0cf.png"
+  },
+  {
+    "revision": "b5b63bbb4b4263182d0053fdad1102e4",
+    "url": "/HealthyDermaBeauty/static/media/HairDo.b5b63bbb.png"
+  },
+  {
+    "revision": "75003232ab7945c85473fa8a44a2e5ac",
+    "url": "/HealthyDermaBeauty/static/media/HairStyle.75003232.png"
+  },
+  {
+    "revision": "2a3adc0a9f82fcb4daef062c8c2374ec",
+    "url": "/HealthyDermaBeauty/static/media/Makeup.2a3adc0a.png"
+  },
+  {
+    "revision": "01301b974cadfc4e72d407608c87aab2",
+    "url": "/HealthyDermaBeauty/static/media/Mehand2.01301b97.png"
+  },
+  {
+    "revision": "9b24b1ad328e95475d80b0d81e634ae2",
+    "url": "/HealthyDermaBeauty/static/media/Mehandi1.9b24b1ad.png"
+  },
+  {
+    "revision": "ecbb6f578f355d6791f65b7dbdc1a292",
+    "url": "/HealthyDermaBeauty/static/media/SareeDropping.ecbb6f57.png"
+  },
+  {
+    "revision": "4c7628617c4b9ac85c206604f516e6a5",
+    "url": "/HealthyDermaBeauty/static/media/SkinTreatment.4c762861.png"
+  },
+  {
+    "revision": "80ced7656c5dc629a327555d5451bbf5",
+    "url": "/HealthyDermaBeauty/static/media/certificate.80ced765.pdf"
+  }
+]);
